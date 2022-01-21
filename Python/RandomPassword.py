092906b86566067a39a9167868a1807ff6bae013
Biscoito2 <lf747562@gmail.com>
2022-01-21 20:26:23 -0300

    Interface nova, para simplificar o uso da ferramenta.

--- Python/RandomPassword.py
+++ Python/RandomPassword.py
@@ -1,10 +1,66 @@
 #Gerador de senhas, primeiro pega a quantidade de caracteres da senha, guarde isso, crie uma variavel.
 #Essa variavel vai ser a senha, faça com que os caracteres maximos sejam o que for escrito no input.
 #E gere caracteres aleatorios, e exiba pro usuario.
-
+import PySimpleGUI as sg
 import random
 import string
-quant_max = int(input("Quantidade máxima de caracteres:"))
-charac = string.ascii_letters
-senha = random.choices(charac, k=quant_max)
-print("".join(senha))
+from subprocess import check_call
+
+sg.theme('reddit')
+
+quant_max = 0
+
+layout = [
+    [sg.Text('Length:'), sg.Input(key='length')],
+    [sg.Text('Numbers:'), sg.Checkbox('', key='checkbox')],
+    [sg.Button('Generate Password')],
+    [sg.Text('Result: ', key='resultado'), sg.Button('Copy')]
+]
+
+janela = sg.Window('Random Password Generator', layout=layout)
+seq = string.ascii_letters
+
+def copy2paste(t):
+    cmd='echo '+t.strip()+'|clip'
+    return check_call(cmd, shell=True)
+
+while True:
+    eventos, valores = janela.read()
+
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Generate Password':
+        print('Apertou!')
+
+        if valores['checkbox']:
+            seq = seq + f'{string.digits}'
+
+
+        try:
+            if int(valores['length']) > 0:
+                q = valores['length']
+                print(q)
+                senha = random.choices(seq, k=int(q))
+                result = ''.join(senha)
+                print(result)
+                janela.Element('resultado').update(f'Result: {result}')
+
+                
+        except Exception:
+            print('Erro!')
+            print(Exception)
+            continue
+
+    if eventos == 'Copy':
+        copy2paste(result)
+        
+
+                          
+
+
+        
+
+    
+            
+        
+
